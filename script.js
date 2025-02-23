0cfcfc7b8c0121b4ae69c4f0fa8da28743bc5a29
ahdconcepts <ahdconcepts@gmail.com>
2025-02-24 01:01:58 +0800

    Update script.js

--- script.js
+++ script.js
@@ -28,13 +28,4 @@ document.addEventListener("DOMContentLoaded", () => {
   menuButton.addEventListener("click", () => {
     menuOptions.classList.toggle("hidden");
   });
-
-  // Progress Bar
-  const progressBar = document.getElementById("progressBar");
-  window.addEventListener("scroll", () => {
-    const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    const scrollTop = document.documentElement.scrollTop;
-    const width = (scrollTop / scrollHeight) * 100;
-    progressBar.style.width = width + "%";
-  });
 });
