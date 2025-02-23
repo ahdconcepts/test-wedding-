fb1b5c5544d665ec61218d2a432a540ac558b552
ahdconcepts <ahdconcepts@gmail.com>
2025-02-24 00:27:30 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,21 +2,21 @@ document.addEventListener("DOMContentLoaded", () => {
   // Slide-in Effect for Sections
   const slideElements = document.querySelectorAll(".slide-in");
   const observer = new IntersectionObserver(
-      (entries) => {
-          entries.forEach((entry) => {
-              if (entry.isIntersecting) {
-                  entry.target.classList.add("visible");
-                  entry.target.classList.remove("exiting");
-              } else if (
-                  entry.boundingClientRect.top < 0 ||
-                  entry.boundingClientRect.bottom > window.innerHeight
-              ) {
-                  entry.target.classList.remove("visible");
-                  entry.target.classList.add("exiting");
-              }
-          });
-      },
-      { threshold: 0.5 }
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+          entry.target.classList.remove("exiting");
+        } else if (
+          entry.boundingClientRect.top < 0 ||
+          entry.boundingClientRect.bottom > window.innerHeight
+        ) {
+          entry.target.classList.remove("visible");
+          entry.target.classList.add("exiting");
+        }
+      });
+    },
+    { threshold: 0.5 }
   );
 
   slideElements.forEach((el) => observer.observe(el));
@@ -26,6 +26,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const menuOptions = document.getElementById("menuOptions");
 
   menuButton.addEventListener("click", () => {
-      menuOptions.classList.toggle("hidden");
+    menuOptions.classList.toggle("hidden");
   });
 });
